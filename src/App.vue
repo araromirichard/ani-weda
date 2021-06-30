cf5b7cec0a6d11cfb8080f5a81d742de8c1c7f88
karosaxy <araromirichard@gmail.com>
2021-06-30 11:32:59 +0100

    UI design started

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <v-app>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <div class="main">
+    <navigation class="navigation" />
+    <router-view />
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 import dB from './firebase/firebaseinit'
+import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'App',
-
+  components: {
+    Navigation
+  },
   data () {
     return {
       APIkey: 'f05769f10f6b590cd6bc26e2df3d315c'
@@ -27,17 +28,24 @@ export default {
     getCityWeather () {
       const firebaseDB = dB.collection('cities')
 
-      firebaseDB.onSnapshot(snap => {
+      firebaseDB.onSnapshot((snap) => {
         snap.docChanges().forEach(async (doc) => {
           if (doc.type === 'added') {
             try {
-              const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${doc.doc.data().city}&units=metrics&APPID=${this.APIkey}`)
+              const response = await axios.get(
+                `http://api.openweathermap.org/data/2.5/weather?q=${
+                  doc.doc.data().city
+                }&units=metrics&APPID=${this.APIkey}`
+              )
               const data = response.data
-              firebaseDB.doc(doc.doc.id).update({
-                currentWeather: data
-              }).then(() => {
-                this.cities.push(doc.doc.data())
-              })
+              firebaseDB
+                .doc(doc.doc.id)
+                .update({
+                  currentWeather: data
+                })
+                .then(() => {
+                  this.cities.push(doc.doc.data())
+                })
             } catch (err) {
               console.log(err)
             }
@@ -48,3 +56,17 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+.main {
+  height: 100vh;
+  .navigation {
+    z-index: 99;
+  }
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="container add-cty">
+    <nav>
+      <span>Add City</span>
+      <div class="right">
+        <i class="far fa-edit"></i>
+        <i class="fas fa-sync"></i>
+        <i class="fas fa-plus"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
